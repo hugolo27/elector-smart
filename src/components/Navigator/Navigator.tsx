e357cf447517217fc00fee86199b7bfbc1c04065
Hugo López <hugolo.27095@gmail.com>
2022-07-12 19:51:32 -0400

    Change HOC for navigation

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable */
+
 import { useNavigate } from 'react-router-dom';
 import React from "react";
 
-// eslint-disable-next-line react/display-name
 const Navigator = (Component: any) => (props: any) => {
     const navigate = useNavigate();
     const navigateToPath = (path: string) => navigate(path);
